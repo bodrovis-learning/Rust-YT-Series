f6ca0783fff118cb9f25e99445c6c0642c9fc5bd
bodrovis <bodrovis@protonmail.com>
2023-11-19 13:55:52 +0200

    lesson 2, add ? example following pr #1

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -24,7 +24,8 @@ fn bin_search(arr: &[i32], desired_value: i32) -> Option<(i32, usize)> {
         let mid = (up_bound + low_bound) / 2;
 
         let mid_value = arr[mid];
-
+        println!("{mid_value}");
+        println!("{mid}");
         match mid_value.cmp(&desired_value) {
             Ordering::Equal => return Some((mid_value, mid)),
             Ordering::Greater => {
@@ -33,6 +34,10 @@ fn bin_search(arr: &[i32], desired_value: i32) -> Option<(i32, usize)> {
                     None => return None // fallback
                 };
             },
+
+            // Lines 30-35 can be replaced with (thanks, @AndrewKraevskii):
+            // Ordering::Greater => up_bound = mid.checked_sub(1)?,
+
             Ordering::Less => low_bound = mid + 1,
         }
 

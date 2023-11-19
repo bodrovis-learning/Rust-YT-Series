4752ea34241e78aaf104fc22c575d89eb039e992
Andrew Kraevskii <andrew.kraevskii@gmail.com>
2023-11-19 10:20:17 +0300

    Change code to use ? operator

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -27,12 +27,7 @@ fn bin_search(arr: &[i32], desired_value: i32) -> Option<(i32, usize)> {
 
         match mid_value.cmp(&desired_value) {
             Ordering::Equal => return Some((mid_value, mid)),
-            Ordering::Greater => {
-                up_bound = match mid.checked_sub(1) {
-                    Some(result) => result,
-                    None => return None // fallback
-                };
-            },
+            Ordering::Greater => up_bound = mid.checked_sub(1)?,
             Ordering::Less => low_bound = mid + 1,
         }
 

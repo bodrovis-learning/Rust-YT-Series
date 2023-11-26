141b030a7692144dcef7c954c9bdc0f84ca58c9c
bodrovis <bodrovis@protonmail.com>
2023-11-26 15:00:51 +0200

    lesson 4

---
+++ lesson4/src/main.rs
@@ -0,0 +1,83 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::io::stdin;
+use std::num::Wrapping;
+
+fn main() {
+    // q1();
+    // q2();
+    // q3();
+    // q4();
+    // q5();
+    // q6();
+    // q7();
+}
+
+// fn q1() {
+//     println!("What is 7 * 8?");
+
+//     let mut input = String::new();
+
+//     stdin()
+//         .read_line(&mut input)
+//         .expect("Error!");
+
+//     if input == "56" {
+//         println!("Correct!");
+//     } else {
+//         println!("Incorrect!");
+//     }
+// }
+
+// fn q2() {
+//     let x: u64 = 4_294_967_296;
+//     let y = x as u32;
+
+//     if x == y as u64 {
+//         println!("x equals y.");
+//     } else {
+//         println!("x does not equal y.");
+//     }
+// }
+
+// fn q3() {
+//     let a: f32 = 5.5028236;
+
+//     println!("{a}");
+// }
+
+// fn q4() {
+//     if 0.1 + 0.2 == 0.3 {
+//         println!("Yes!");
+//     } else {
+//         println!("No!");
+//     }
+// }
+
+// fn q5() {
+//     let mut counter: i8 = 0;
+
+//     loop {
+//         println!("{counter}");
+//         counter += 1;
+//     }
+// }
+
+// fn q6() {
+//     let s = "Привет!";
+
+//     println!("String {} has {} characters.", s, s.len());
+// }
+
+// fn call_me(n: u64, _: i32, c: u32) -> u64 {
+//     println!("{c}");
+//     n * 2
+// }
+
+// fn q7() {
+//     let one: i32 = 1;
+//     let c: i8 = -3;
+//     let n = call_me(one as _, 3, c as _);
+
+//     println!("{n}");
+// }
